1d51cc74925676a0a330a2dbb21741eaa34c29f2
martinrusev <martinrusev@zoho.com>
2011-12-31 22:42:43 +0000

    Log API updates, LogAPI test suite

--- amon/api/log.py
+++ amon/api/log.py
@@ -1,29 +1,41 @@
 from time import time
-from amon.backends.mongodb import backend
+from amon.api.models import LogsAPIModel, CommonAPIModel
 
 class Log(object):
 
 	def __init__(self):
-		self.levels = ('warning', 'error', 'info', 'critical', 'debug')
+		self.model = LogsAPIModel()
+		self.common_model = CommonAPIModel()
+
+	# Checks the tags in the database and adds them if they are new entries
+	def check_tags(self, tags):
+
+		if isinstance(tags, list):
+			for el in tags:
+				self.model.upsert_tag(el)
+						
+		elif isinstance(tags, str) or isinstance(tags, unicode):
+			self.model.upsert_tag(tags)
 
 	def __call__(self, *args, **kwargs):
 
 		log_dict = args[0]
 
 		try:
-			level = log_dict.get('level')
-			if level not in self.levels:
-				level = 'notset'
+			tags = log_dict.get('tags')
 		except: 
-			level = 'notset'
+			tags = None
 		
 		message = log_dict.get('message', '')
 
 		now = int(time())
 
-		entry = {'time': now, 'message': message, 'level': level}
+		self.check_tags(tags)
+
+		entry = {'time': now, 'message': message, 'tags': tags}
 		
 		# Add the data to a separate field, for easy searching 
+		# TODO - support for dictionaries with more than 1 level
 		if isinstance(message, dict):
 			_searchable = ":".join(message.keys())
 		elif isinstance(message, list):
@@ -32,17 +44,6 @@ class Log(object):
 			_searchable = message
 		
 		entry['_searchable'] = _searchable
-		
 
-		backend.store_entry(entry, 'logs')
-
-
-		# TODO - refactor it at some point, when expanding the API
-		unread = backend.get_collection('unread')
-		unread_counter = unread.find({"id": 1}).count()
-
-		if unread_counter == 0:
-			_counter = {'id':1, 'exceptions': 0, 'logs': 1}
-			unread.save(_counter)
-		else:
-			unread.update({"id": 1}, {"$inc": {"logs": 1}})
+		self.model.save_log(entry)
+		self.common_model.upsert_unread('logs')
